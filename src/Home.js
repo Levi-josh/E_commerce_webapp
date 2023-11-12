b9ab5f8b56274697cd4c8e6336866a94e507e461
Levi-josh <levijoshuakelly@gmail.com>
2023-11-12 20:35:40 -0200

    updated

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
     return (
         <div className='home'>
 
-            <header className='fixed flex justify-between items-center h-14 px-3 w-full m-0 bg-white'>
-                <h1 className='font-bold'>#Soundcity</h1>
+            <header className='fixed flex justify-between items-center h-14 sm:h-16 md:h-20 px-3 w-full m-0 bg-white z-10 shadow-lg'>
+                <h1 className='font-bold'>#Glamour Grove</h1>
                 <ul className='hidden md:flex'>
                     <li>Home</li>
                     <li>Shop</li>
@@ -22,22 +22,50 @@ const Home = () => {
                     <FaAddressBook className='hidden md:block' />
                 </div>
             </header>
-            <div className='overflow-hidden w-120 flex justify-center items-center sm:w-150 md:w-130 lg:w-130  pt-16  m-auto'>
+            <div className='overflow-hidden w-120 flex justify-center items-center sm:w-130 md:w-130 lg:w-130  pt-16 sm:pt-28 md:pt-32 lg:pt-36  m-auto'>
                 <img src='https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph' className='w-full  md:h-96 bg-no-repeat bg-cover bg-center' />
+                <div className='absolute mt-80 flex '>
+                    <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
+                    <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
+                    <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
+                    <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
+                    <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
+                </div>
             </div>
             <section>
                 <div className='mt-10 text-center mb-8 md:flex lg:w-130 md:m-auto md:w-130 md:justify-between md:items-center md:mt-12 md:mb-12'>
                     <h1 className='text-2xl font-bold'>simply unique/better  </h1>
-                    <p><strong>soundcity</strong> is a gift and decoration store based in hcmc, vietnie since 2019.</p>
+                    <p><strong>Glamour Grove</strong> is a gift and decoration store based in hcmc, vietnie since 2019.</p>
                 </div>
                 <div className='flex flex-col md:grid md:grid-cols-2   gap-8 md:gap-5  lg:gap-x-5 xl:gap-x-5 xl:gap-y-5 md:justify-items-center md:w-130 m-auto lg:w-130   justify-center items-center'>
-                    <div>
-
+                    <div className='md:w-full'>
+                        <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
+                            <h1 className='font-bold md:text-lg lg:text-xl'>clothes</h1>
+                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                        </div>
+                        <img src='https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg' className='w-72 h-48 lg:w-full lg:h-72 md:w-full sm:w-96 xl:h-100 sm:h-56' />
                     </div>
-                    <img src='https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg' className='w-72 h-48 lg:w-full md:h-72 md:w-full sm:w-96 xl:h-100 sm:h-56' />
-                    <img src={homeimage} className='w-72 h-48 lg:w-full md:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56' />
-                    <img src={homeimage} className='w-72 h-48 lg:w-full md:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56' />
-                    <img src={homeimage} className='w-72 h-48 lg:w-full md:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56 ' />
+                    <div className='md:w-full'>
+                        <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
+                            <h1 className='font-bold md:text-lg lg:text-xl'>Glasses</h1>
+                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                        </div>
+                        <img src={homeimage} className='w-72 h-48 lg:w-full lg:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56' />
+                    </div>
+                    <div className='md:w-full'>
+                        <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
+                            <h1 className='font-bold md:text-lg lg:text-xl'>Watches</h1>
+                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                        </div>
+                        <img src={homeimage} className='w-72 h-48 lg:w-full lg:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56' />
+                    </div>
+                    <div className='md:w-full'>
+                        <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
+                            <h1 className='font-bold md:text-lg lg:text-xl'>Shoes</h1>
+                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                        </div>
+                        <img src={homeimage} className='w-72 h-48 lg:w-full lg:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56 ' />
+                    </div>
 
                 </div>
                 {/*overflow hidden divs*/}
@@ -59,16 +87,23 @@ const Home = () => {
                 {/*ends*/}
 
                 {/*about starts*/}
-                <div className='flex w-full'>
+                <div className='md:flex md:p-10 shadow-lg w-110 m-auto shadow-black mt-10 border-t-2 p-5 sm:p-10 sm:px-20 '>
 
-                    <img src='https://img.freepik.com/free-vector/fashion-business-man-accessories-set_1284-16521.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph' className='h-72 w-full' />
-                    <div className='px-20'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum illum quam numquam impedit quo? Facere, optio! Voluptatum, possimus? Voluptatum corrupti error saepe illo ut dignissimos corporis itaque modi beatae?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nemo quam molestias quos sapiente nihil est, sed esse iure facilis voluptatum ad dolor architecto eos iste provident autem tempore quisquam.
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6XwgxBgBRVxNaZM2ZGtxV8Jy8YSEKG8_Vrw&usqp=CAU' className='w-full md:h-72 h-64' />
+                    <div className='w-full'>
+                        <h1>
+                            Special Fashon sales
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum illum quam numquam impedit quo? Facere, optio! Voluptatum, possimus? Voluptatum corrupti error.
+
+                        </p>
+                        <h1>30% OFF</h1>
+                        <button>Shop Now</button>
                     </div>
                 </div>
                 {/*about ends*/}
-                <div className='grid grid-cols-2 w-130 gap-4 justify-items-center m-auto lg:flex mt-5 md:gap-5'>
+                <div className='grid grid-cols-2 w-110 gap-4 justify-items-center m-auto lg:flex mt-5 md:gap-5'>
                     <div className=' bg-yellow-800 w-full h-36 pt-4 pl-4 lg:h-44 '>
                         <FaCar />
                         <h2>Free shipping</h2>
