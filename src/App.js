9179cb80591fc6328a2fd8edf76e462d9cad63b9
Levi-josh <levijoshuakelly@gmail.com>
2023-11-15 21:38:59 -0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 
 import './App.css';
+import { Route, Routes, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from './Home';
+import Cart from './Cart';
+import ScrollToTOP from './ScrollToTOP.js';
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+
+    <Route path='/' element={<ScrollToTOP />} >
+      <Route index element={<Home />} />
+      <Route path='/cart' element={<Cart />} />
+    </Route>
+
+  ));
+
+
   return (
-    <div className="App">
-      <Home />
-
-
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaBars, FaSearch, FaBell, FaAddressBook, FaCar, FaWallet, FaLock, FaPhone } from 'react-icons/fa'
+import { FaBars, FaSearch, FaBell, FaAddressBook, FaCar, FaWallet, FaLock, FaPhone, FaShoppingCart } from 'react-icons/fa'
 import homeimage from './hotdog image.jpg'
 import { Link, NavLink } from 'react-router-dom'
 
@@ -13,25 +13,26 @@ const Home = () => {
                     <li>Home</li>
                     <li>Shop</li>
                     <li>Product</li>
-                    <li>Contact us</li>
+                    <li>About</li>
                 </ul>
                 <div className='flex gap-3'>
-                    <FaSearch />
+                    <FaShoppingCart />
                     <FaBell />
-                    <FaBars className='md:hidden' />
+                    <FaBars className='md:hidden ' />
                     <FaAddressBook className='hidden md:block' />
                 </div>
             </header>
-            <div className='overflow-hidden w-120 flex justify-center items-center sm:w-130 md:w-130 lg:w-130  pt-16 sm:pt-28 md:pt-32 lg:pt-36  m-auto  '>
-                <img src='https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph' className='w-full  md:h-96 bg-no-repeat bg-cover bg-center ' />
+            <div className='overflow- w-120 flex gap-10 justify-center items-center sm:w-130 md:w-130 lg:w-130  pt-16 sm:pt-28 md:pt-32 lg:pt-36  m-auto  '>
+                <img src='https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph' className='w-full   md:h-96 bg-no-repeat bg-cover bg-center   ' />
+
 
             </div>
-            <div className='absolute flex m-auto z-10 w-full  justify-center '>
-                <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
-                <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
-                <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
-                <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
-                <div className='  bg-blue-600 w-3 h-3 rounded-lg'></div>
+            <div className='absolute flex m-auto  w-full  justify-center mt-3 gap-1 md:gap-2'>
+                <div className='   w-3 h-3 rounded-lg border border-black '></div>
+                <div className='   w-3 h-3 rounded-lg border border-black '></div>
+                <div className='  w-3 h-3 rounded-lg border border-black'></div>
+                <div className='   w-3 h-3 rounded-lg border border-black'></div>
+                <div className='   w-3 h-3 rounded-lg border border-black'></div>
             </div>
             <section>
                 <div className='mt-10 text-center mb-8 md:flex lg:w-130 md:m-auto md:w-130 md:justify-between md:items-center md:mt-12 md:mb-12'>
@@ -42,28 +43,28 @@ const Home = () => {
                     <div className='md:w-full'>
                         <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
                             <h1 className='font-bold md:text-lg lg:text-xl'>clothes</h1>
-                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                            <p className=' hover:cursor-pointer md:text-lg'>see more...</p>
                         </div>
                         <img src='https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg' className='w-72 h-48 lg:w-full lg:h-72 md:w-full sm:w-96 xl:h-100 sm:h-56' />
                     </div>
                     <div className='md:w-full'>
                         <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
                             <h1 className='font-bold md:text-lg lg:text-xl'>Glasses</h1>
-                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                            <p className=' hover:cursor-pointer md:text-lg '>see more...</p>
                         </div>
                         <img src={homeimage} className='w-72 h-48 lg:w-full lg:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56' />
                     </div>
                     <div className='md:w-full'>
                         <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
                             <h1 className='font-bold md:text-lg lg:text-xl'>Watches</h1>
-                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                            <p className=' hover:cursor-pointer md:text-lg'>see more...</p>
                         </div>
                         <img src={homeimage} className='w-72 h-48 lg:w-full lg:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56' />
                     </div>
                     <div className='md:w-full'>
                         <div className='absolute bg-yellow-800 w-36 mt-3 md:w-40 lg:w-44 xl:w-48 pl-3 md:pl-4 lg:pl-5 lg:mt-5 lg:py-1 ml-2 lg:ml-4 text-white'>
                             <h1 className='font-bold md:text-lg lg:text-xl'>Shoes</h1>
-                            <p className='underline hover:cursor-pointer md:text-lg'>see more...</p>
+                            <p className=' hover:cursor-pointer md:text-lg'>see more...</p>
                         </div>
                         <img src={homeimage} className='w-72 h-48 lg:w-full lg:h-72 md:w-full xl:h-100 sm:w-96 sm:h-56 ' />
                     </div>
@@ -73,9 +74,9 @@ const Home = () => {
 
 
                 <div>
-                    <div className='flex justify-between'>
-                        <h1>NEW ARRIVAL</h1>
-                        <p className='hidden md:block '>see more...</p>
+                    <div className='flex justify-between items-center'>
+                        <h1 className='font-bold my-2 ml-3 sm:ml-4 md:ml-5  text-lg sm:my-4  md:my-5 lg:my-6 lg:text-2xl lg:ml-6 md:text-xl'>New<span className='flex flex-wrap'> Arrivals</span></h1>
+                        <p className='hidden md:block lg:mt-10 md:mr-5 lg:mr-6 md:mt-8 lg:text-lg'>more products...</p>
                     </div>
                     <div className='overflow-div '>
                         <img src='https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg' className='w-10 md:w-20  lg:w-15' />
@@ -83,7 +84,7 @@ const Home = () => {
                         <img src={homeimage} className='w-10 md:w-20  lg:w-15' />
                         <img src={homeimage} className='w-10 md:w-20  lg:w-15' />
                     </div>
-                    <p className='md:hidden'>see more...</p>
+                    <p className='md:hidden mt-2 ml-3 sm:ml-4'>more products...</p>
                 </div>
                 {/*ends*/}
 
@@ -91,16 +92,18 @@ const Home = () => {
                 <div className='md:flex md:p-10 shadow-lg w-110 m-auto shadow-black mt-10 border-t-2 p-5 sm:p-10 sm:px-20 '>
 
                     <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6XwgxBgBRVxNaZM2ZGtxV8Jy8YSEKG8_Vrw&usqp=CAU' className='w-full md:h-72 h-64' />
-                    <div className='w-full'>
-                        <h1>
+                    <div className='w-full pt-4 text-center md:pt-0 md:pl-8'>
+                        <h1 className='font-bold text-lg md:text-2xl lg:text-3xl'>
                             Special Fashon sales
                         </h1>
-                        <p>
+                        <p className='mt-3 md:mt-4 lg:mt-8 md:text-lg'>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum illum quam numquam impedit quo? Facere, optio! Voluptatum, possimus? Voluptatum corrupti error.
 
                         </p>
-                        <h1>30% OFF</h1>
-                        <button>Shop Now</button>
+                        <div className='md:flex justify-center items-center gap-5 mt-6 lg:mt-10'>
+                            <h1 className='w-30 border border-dashed  border-black mt-3 md:mt-0 lg:w-20 md:w-24 md:rounded-lg md:h-10 flex justify-center items-center font-bold md:text-lg h-8  '>30% OFF</h1>
+                            <button className='w-40 bg-yellow-800 mt-4 text-white md:w-24 md:mt-0 lg:w-20 md:h-10 md:rounded-lg font-semibold h-8 md:text-lg'>Shop Now</button>
+                        </div>
                     </div>
                 </div>
                 {/*about ends*/}
