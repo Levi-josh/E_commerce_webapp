4b6e72c3654eeeebc98683af55f80669c33c221a
Levi-josh <levijoshuakelly@gmail.com>
2023-12-04 19:37:33 -0200

    updated

--- src/Shopcart.js
+++ src/Shopcart.js
@@ -4,8 +4,8 @@ import { FaTimes } from 'react-icons/fa'
 const Shopcart = () => {
     return (
         <div className='pt-6 sm:pt-8'>
-            <div className='lg:flex items-start lg:w-110 m-auto gap-10 lg:mt-10 '>
-                <div className='lg:w-full lg:flex lg:flex-col lg:justify-center items-center  '>
+            <div className='lg:flex items-start lg:w-110 m-auto gap-5 lg:mt-10 '>
+                <div className=' lg:w-130 lg:flex lg:flex-col lg:justify-center items-center  '>
 
                     <ul className='border-b border-black pb-6 pl-3 sm:pl-5 sm:pb-7  lg:grid-cols-6 lg:grid lg:pb-0 lg:pl-0  lg:w-full'>
                         <li className='sm:text-lg lg:col-span-3'>Product</li>
@@ -16,7 +16,7 @@ const Shopcart = () => {
 
                     </ul>
 
-                    <div className='flex gap-5 justify-center border-b lg:w-full   border-gray-400 mt-5 sm:mt-8  sm:pb-8  sm:m-auto pb-5 lg:grid lg:grid-cols-7 lg:gap-x-28  '>
+                    <div className='flex gap-5 justify-center border-b lg:w-full   border-gray-400 mt-5 sm:mt-8  sm:pb-8  sm:m-auto pb-5 lg:grid lg:grid-cols-7  '>
                         <img src='https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg' className='w-40 sm:w-52 lg:hidden ' />
                         <div className='grid grid-cols-2 place-items-center gap-x-10 lg:hidden  '>
                             <h1>watch</h1>
@@ -46,9 +46,9 @@ const Shopcart = () => {
                     </div>
                 </div>
 
-                <div className='w-110 border sm:w-140 md:w-140 border-black m-auto px-5 mt-10 sm:mt-14 md:px-10 sm:px-10  py-3 sm:py-7 lg:px-8 lg:py-6 lg:mt-0 lg:w-140'>
+                <div className='w-110 border sm:w-140 md:w-140 border-black m-auto px-5 mt-10 sm:mt-14 md:px-10 sm:px-10  py-3 sm:py-7 lg:px-3 xl:px-8 lg:py-2 xl:py-6 lg:mt-0 lg:w-22'>
                     <h1 className='font-bold sm:text-lg md:text-xl lg:text-2xl '>Cart summary</h1>
-                    <div className='flex  border border-black mt-5 justify-between p-3  '>
+                    <div className='flex  border border-black mt-5 justify-between p-3 lg:mt-4 xl:mt-5 '>
                         <div className='flex gap-2'>
                             <input type='radio' />
                             <p className='sm:text-lg lg:text-xl'>Free shipping</p>
