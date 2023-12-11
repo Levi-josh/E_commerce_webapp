32640e956e8111915fd3704533e951e631534841
Levi-josh <levijoshuakelly@gmail.com>
2023-12-11 12:06:45 -0200

    updated

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaArrowDown } from 'react-icons/fa'
+import { FaArrowDown, FaTimes } from 'react-icons/fa'
 
 const Checkout = () => {
     return (
@@ -90,7 +90,35 @@ const Checkout = () => {
                     </div>
                 </div>
             </div>
-            <div></div>
+
+
+
+
+            <div className='w-110 border border-black px-5'>
+                <h1>Order Summary</h1>
+                <div className='flex gap-5  border-b lg:w-full   border-gray-400  pb-5 mb-3 mt-5    '>
+                    <img src='https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-fernando-arcos-190819.jpg&fm=jpg' className='w-40 sm:w-52  ' />
+                    <div className='grid grid-cols-2 place-content-between  '>
+                        <h1>watch</h1>
+                        <h1>$100</h1>
+                        <div className='w-16 h-8 border border-black text-center'>-3+</div>
+                        <FaTimes />
+
+                        <h1>subtotal:</h1>
+                        <h1>$300</h1>
+
+                    </div>
+                </div>
+                <div className='flex justify-between items-center border-gray-400 border-b  py-3 mt-3'>
+                    <h1 className='sm:text-lg lg:text-xl'>Subtotal</h1>
+                    <h1 className='sm:text-lg lg:text-xl'>$1200</h1>
+                </div>
+                <div className='flex justify-between items-center py-3 my-3'>
+                    <h1 className='sm:text-lg lg:text-xl'>Total</h1>
+                    <h1 className='sm:text-lg lg:text-xl'>$2000</h1>
+                </div>
+            </div>
+
         </div>
     )
 }
