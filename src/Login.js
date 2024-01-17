23d121d1836764244a00016fc5f2cc26b18571a3
Levi-josh <levijoshuakelly@gmail.com>
2024-01-17 08:20:26 -0200

    updated

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FaArrowLeft } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
 
 const Login = () => {
     return (
         <div className='flex justify-center items-center w-full  h-screen '>
+            <NavLink to='../' path='relative'> <FaArrowLeft className='font-bold absolute top-5 left-5 sm:top-10 sm:left-10  text-xl sm:text-2xl lg:hidden' ></FaArrowLeft></NavLink>
+
             <div className='w-130 sm:w-140  md:w-130 lg:w-140 md:h-96  justify-center md:items-center   items-start flex flex-col md:flex-row shadow-xl gap-3 sm:gap-6 md:gap-10 lg:gap-12 '>
                 <div className='w-full md:h-full'>
                     <img src='https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph' className='h-full w-full        bg-no-repeat bg-cover bg-center   ' />
