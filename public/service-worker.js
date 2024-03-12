986cbce5acb73cf124b661d55cd638b45dbea22f
Levi-josh <levijoshuakelly@gmail.com>
2024-03-12 16:37:54 -0200

    updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('push', (event) => {
       console.log( notificationPayload)
       console.log('notification')
     event.waitUntil(
-      self.registration.showNotification(notification.title, {body:notification.body})
+      self.registration.showNotification(notification.title, {body:notification.body,icon:notification.body||'https://img.freepik.com/free-photo/lot-different-clothes-hanging-wardrobe_181624-16122.jpg?size=626&ext=jpg&ga=GA1.1.103364066.1699032278&semt=sph'})
     );
   });
   console.log('notification')
